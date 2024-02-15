ec58dd45472914eeb6fbf46c9c8c72d1f475cbab
Vishal <vishal.ranjan7777@gmail.com>
2024-02-14 19:19:54 -0600

    Finish send message functionality between two users

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -9,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incomign request with JSON Payloads (from req.body)
-
+app.use(cookieParser()); // to parse the incomming request with cookies (from req.cookies)
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 
 // app.get('/', (req, res) => {
 //     // root route http://localhost:5000
