df92f85f119aa49775d8182c8a6eff493e373338
Vishal <vishal.ranjan7777@gmail.com>
2024-02-20 22:06:08 -0600

    Add routes to pages usign react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
 import { Login } from './pages/login/Login.jsx';
 import { Signup } from './pages/signup/Signup.jsx';
 import { Home } from './pages/home/Home.jsx';
 
-
 function App() {
     return (
         <div className='p-4 h-screen flex items-center justify-center'>
-            <Home />         
+            <Routes>
+                <Route path='/' element={ <Home /> } />
+                <Route path='/signup' element={ <Signup /> } />
+                <Route path='/login' element={ <Login /> } />
+            </Routes>     
         </div>
     )
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GenderCheck } from './GenderCheck'
+import { Link } from 'react-router-dom'
 
 export const Signup = () => {
   return (
@@ -63,9 +64,9 @@ export const Signup = () => {
                 </div>
 
                 <GenderCheck />
-                <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                <Link to={'/login'} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                     Already have an account? 
-                </a>
+                </Link>
 
                 <div>
                     <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
