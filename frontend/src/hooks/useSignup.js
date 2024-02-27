84c527b7432553aff361a875f702615d7a52e1fd
Vishal <vishal.ranjan7777@gmail.com>
2024-02-26 21:10:50 -0600

    Add protected routes for navigation using authcontext
    only auth user can be able to visit the home page

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
+
+    const { setAuthUser } = useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -20,6 +23,13 @@ const useSignup = () => {
 			if (data.error) {
 				throw new Error(data.error);
 			}
+
+            //local storage
+            localStorage.setItem("chat-user", JSON.stringify(data));
+
+            //context
+            setAuthUser(data);
+
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
