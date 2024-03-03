9c005e1f646b433576f8c3f16a45dab06b94375c
Vishal <vishal.ranjan7777@gmail.com>
2024-03-03 12:51:02 -0600

    Blue background when a chat is selected, also change the message container to welcome message after logout and log back in

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation'
 
 export const Conversation = ({key, conversation, emoji, lastIdx}) => {
-  return (
-    <>
-        <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
-            <div className="avatar online">
-                <div className="w-12 rounded-full">
-                    <img
-                        src={conversation.profilePic}
-                        alt="User Image"
-                    />
+    const {selectedConversation, setSelectedConversation} = useConversation(); //zustand store
+    const isSelected = selectedConversation?._id === conversation?._id;
+    return (
+        <>
+            <div className={
+                `flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+                ${isSelected ? "bg-sky-500" : ""}
+            `}
+                onClick={() => setSelectedConversation(conversation)}
+            >
+                <div className="avatar online">
+                    <div className="w-12 rounded-full">
+                        <img
+                            src={conversation.profilePic}
+                            alt="User Image"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col flex-1">
+                    <div className="flex gap-3 justify-between">
+                        <p className='font-fold text-gray-200'>{conversation.fullName}</p>
+                        <span className='text-xl'>{emoji}</span>
+                    </div>
                 </div>
             </div>
 
-            <div className="flex flex-col flex-1">
-                <div className="flex gap-3 justify-between">
-                    <p className='font-fold text-gray-200'>{conversation.fullName}</p>
-                    <span className='text-xl'>{emoji}</span>
-                </div>
-            </div>
-        </div>
-
-        {!lastIdx && <div className='divider my-0 py-0 h-1' />}
-    </>
-  )
+            {!lastIdx && <div className='divider my-0 py-0 h-1' />}
+        </>
+    )
 }
