4f3cd8c59054c9f1421e85646877790796709cff
Vishal <vishal.ranjan7777@gmail.com>
2024-03-03 10:30:08 -0600

    display all chat person with random emojis

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Conversation } from './Conversation'
 import useGetConversation from '../../hooks/useGetConversation'
+import { getRandomEmoji } from '../../hooks/utils/emojis';
 
 export const Conversations = () => {
     const {loading, conversations } = useGetConversation();
-    console.log("Conversations:", conversations);
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations?.map((conversation, idx) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation} 
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.length - 1 }
+                />
+            ))}
+            {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
         </div>
     )
 }
