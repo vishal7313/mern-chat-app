f80cc5494ec6c7073fc84b4e050d70a75452539a
Vishal <vishal.ranjan7777@gmail.com>
2024-03-04 17:08:24 -0600

    Connect frontend and backend to get all the messages

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import { Message } from './Message'
+import useGetMessages from '../../hooks/useGetMessages';
 
 export const Messages = () => {
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-    </div>
-  )
+    const {messages, loading } = useGetMessages();
+    console.log("messages:", messages)
+    return (
+        <div className='px-4 flex-1 overflow-auto'>
+            
+        </div>
+    )
 }
 
 export default Messages;
