53625902923fdf7fe94383471d9065735c06c90e
Vishal <vishal.ranjan7777@gmail.com>
2024-03-05 22:55:27 -0600

    Create message skeleton for loading message container

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Message } from './Message'
 import useGetMessages from '../../hooks/useGetMessages';
+import MessageSkeleton from '../skeleton/MessageSkeleton';
 
 export const Messages = () => {
     const {messages, loading } = useGetMessages();
     console.log("messages:", messages)
     return (
         <div className='px-4 flex-1 overflow-auto'>
-            
+            {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
         </div>
     )
 }
