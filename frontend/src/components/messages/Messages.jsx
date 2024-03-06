f9333d2278889af8fad077cf72a139fc81ebb546
Vishal <vishal.ranjan7777@gmail.com>
2024-03-05 23:04:13 -0600

    Add start a conversation message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -9,6 +9,13 @@ export const Messages = () => {
     return (
         <div className='px-4 flex-1 overflow-auto'>
             {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+            {
+                /* Start a message div */
+                !loading && messages.length === 0 && (
+                    <p className='text-center'> Send a message to start a conversation </p>
+                )
+            }
         </div>
     )
 }
