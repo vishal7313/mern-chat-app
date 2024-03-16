05f4487422084332c164c2bac5624c17415321bf
Vishal <vishal.ranjan7777@gmail.com>
2024-03-16 16:58:22 -0500

    Add notification sounds when a new message pops up

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
+import notificationSounds from "../assets/sounds/notification.mp3";
+
 const useListenMessages = () => {
     const { socket } = useSocketContext();
 	const { messages, setMessages } = useConversation();
@@ -10,6 +12,8 @@ const useListenMessages = () => {
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true;
+            const sound = new Audio(notificationSounds);
+            sound.play();
             setMessages([...messages, newMessage]);
         })
 
